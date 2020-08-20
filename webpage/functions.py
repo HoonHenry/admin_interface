f3934bf6d66348ec8a764ea6cb7b8d6835d4ef1a
HoonCantona <dreampath88@gmail.com>
2020-08-21 00:53:48 +0900

    uploading

---
+++ webpage/functions.py
@@ -0,0 +1,30 @@
+# info = {"id": 'asdf', "password": 'asdfasdf', "phone_number": "010-1234-1234", "email": "asdf@asdf.com"}
+
+
+# get inputs from html form
+def get_userinfo(**kwargs):
+    user = kwargs
+    print(user)
+    return user
+
+
+# def get_userinfos(user):
+#     users = []
+#     user_id = infos['ID']
+#     for info in infos:
+#         user = get_userinfo(info)
+#         user_infos.append(user)
+#         print(f"A {user_id}'s information is stored")
+#
+#     print(user_infos)
+#     return user_infos
+
+
+
+def show_infos():
+    pass
+
+
+
+
+
---
+++ webpage/main.py
@@ -0,0 +1,35 @@
+from flask import render_template, Flask, request, redirect
+from webpage.functions import *
+
+
+app = Flask("adminEnv")
+
+db = {}
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route("/result")
+def result():
+    user_id = request.args.get('userid')
+    user_pw = request.args.get('userpw')
+    user_name = request.args.get('username')
+    user_email = request.args.get('useremail')
+    user_phone = request.args.get('phonenum')
+    clearance = request.args.get('rank')
+    user = get_userinfo(id=user_id, pw=user_pw, name=user_name,
+                        email=user_email, phone=user_phone, cl=clearance)
+
+    return render_template('result.html', userid=user['id'],
+                           username=user['name'])
+
+
+app.run(host="0.0.0.0")
